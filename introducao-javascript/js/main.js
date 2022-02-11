49934b93b7c84e5bc735477816d04ca8f94e833c
wagnergbriel <wagnergabriel220596@gmail.com>
2022-02-10 21:21:26 -0300

    :hammer: adicionar funcao main

--- introducao-javascript/js/main.js
+++ introducao-javascript/js/main.js
@@ -1,2 +1,39 @@
 var titulo = document.querySelector("#titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+const listaDeDadosDosPacientes = document.querySelectorAll(".paciente");
+
+function main() {
+
+    for (var i = 0; i < listaDeDadosDosPacientes.length; i++) {
+        let peso;
+        let altura;
+        let pesoEhValido = true;
+        let alturaEhValida = true;
+        let imc;
+
+        peso = listaDeDadosDosPacientes[i].querySelector(".info-peso").textContent;
+        altura = listaDeDadosDosPacientes[i].querySelector(".info-altura").textContent;
+        
+        if(peso <= 0 || peso >= 400) {
+            pesoEhValido = false; 
+            listaDeDadosDosPacientes[i].querySelector(".info-imc").textContent = "Peso Inválido!";
+            listaDeDadosDosPacientes[i].classList.add("paciente-invalido");
+        }
+
+        if(altura <= 0 || altura >= 3.0){
+            alturaEhValida = false;
+            listaDeDadosDosPacientes[i].querySelector(".info-imc").textContent = "Altura Inválida!";
+            listaDeDadosDosPacientes[i].classList.add("paciente-invalido");
+        }
+
+        if(pesoEhValido && alturaEhValida){
+            imc = peso / (altura * altura);
+            listaDeDadosDosPacientes[i].querySelector(".info-imc").textContent = imc.toFixed(2);
+        }
+    } 
+}
+
+//Construir função que insere novos dados na tabela.
+
+main();
